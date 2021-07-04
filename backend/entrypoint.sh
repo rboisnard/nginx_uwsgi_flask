5fb08691552d165dc1d45e094ed16bde99dbf846
rboisnard <50302773+rboisnard@users.noreply.github.com>
2021-07-04 13:50:24 +0200

    debug deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# get the port from env var $PORT set randomly by heroku
-if [ -z $PORT ]; then
-  PORT=5555
-fi
+# override the port from env var $PORT set randomly by heroku
+PORT=5555
 echo "running on port $PORT"
 . /.venv/bin/activate
 uwsgi --yaml app.yml
