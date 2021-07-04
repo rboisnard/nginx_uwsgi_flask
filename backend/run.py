316400565418e437445a5b5f968c047855c52d1b
rboisnard <50302773+rboisnard@users.noreply.github.com>
2021-07-04 13:18:32 +0200

    use dynamic url in backend to allow forwarding

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request, get_flashed_messages, redirect
+from flask import Flask, render_template, flash, request, get_flashed_messages, redirect, url_for
 from wtforms import Form, validators, StringField
 
 import logging
@@ -14,7 +14,6 @@ app.config['SECRET_KEY'] = os.urandom(24)
 
 HOST = "0.0.0.0"
 PORT_FLASK = os.getenv('PORT', 5555)
-APP_NAME = os.getenv('APP_NAME', "https://nginx-uwsgi-flask.herokuapp.com/")
 
 # ------------logging---------------------------------------------------------------
 logging.basicConfig(level=logging.INFO)
@@ -28,20 +27,20 @@ class Codec:
         self.long2short = {}
         self.short2long = {}
 
-    def encode(self, long_url):
+    def encode(self, base_url, long_url):
         while long_url not in self.long2short:
             short_url = ''.join(choice(self.alphabet) for _ in range(6))
             if short_url not in self.short2long:
                 self.short2long[short_url] = long_url
                 self.long2short[long_url] = short_url
-        return APP_NAME + self.long2short[long_url]
+        return f"{base_url}{self.long2short[long_url]}"
 
-    def decode(self, short_url):
+    def decode(self, base_url, short_url):
         suffix = short_url[-6:]
         if suffix in self.short2long:
             return self.short2long[suffix]
         else:
-            return APP_NAME
+            return base_url
 
 # codec needs to be accessible to wsgi caller
 codec = Codec()
@@ -62,7 +61,7 @@ def home():
 
         if form.validate():
             logger.info(f"[OK] Form is validated: {name}")
-            flash(codec.encode(name), 'info')
+            flash(codec.encode(url_for("home", _external=True), name), 'info')
         else:
             logger.info(f"[Error] Form is NOT validated: {name}")
             flash('Please enter long URL in the correct format', 'error')
@@ -75,10 +74,8 @@ def home():
 @app.route("/<short_url>", methods=['GET', 'POST'])
 def togo(short_url):
     logger.info(f"[Redirect] Redirection request: {short_url}")
-    long_url = codec.decode(short_url)
+    long_url = codec.decode(url_for("home", _external=True), short_url)
     logger.info(f"[Redirect2] Going to redirect to: {long_url}")
-    if long_url == APP_NAME:
-        flash(f"Short URL {APP_NAME + short_url} not found in DB, please try another one", 'error')
     return redirect(long_url)
 
 # ---------------------------------------------------------------------------------
